5151d40a4517b104bed43c3a0426c049912ffd4c
Souhaïl Bechchari <146872818+SouhailBechchari@users.noreply.github.com>
2023-10-08 13:24:25 +0100

    Add files via upload

---
+++ EX15.py
@@ -0,0 +1,9 @@
+A = float(input("tapez la 1er note"))
+B = float(input("tapez la 2eme note"))
+C = float(input("tapez la 3eme note"))
+X = (A + B + C )/ 3
+if  X>=16 :print("tres bien")
+if X>= 14 and X<16  :print("bien")
+if X>=12 and X< 14 :print("assez bien")
+if X>=10 and X < 12 :print("passable")
+if  X<10 :print("Insuffisant")S
